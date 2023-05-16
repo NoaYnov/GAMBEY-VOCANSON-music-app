37afd605844455412ce2d62dfd03cc27362cf0a2
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-05-16 13:23:58 +0200

    request

--- script/main.js
+++ script/main.js
@@ -32,7 +32,7 @@ async function SpotifyAuthentification() {
   .then(response => response.json())
   .then(data => {
     Token = data;
-    console.log(Token);
+    // console.log(Token);
   })
   .catch(error => console.log(error));
 
@@ -51,8 +51,76 @@ async function GetTracks(searchTerm) {
       })
       .then(data => {
         Data = data;
-        console.log(Data);
+        Dropdown(Data);
+        // console.log(Data.tracks)
       })
       .catch(error => console.log(error));
 }
+
+String.prototype.replaceAt = function(index, replacement) {
+  return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
+String.prototype.Capitalize = function() {
+  return this.replaceAt(0, this.charAt(0).toUpperCase());
+}
+
+function GetInput() {
+  const input = document.querySelector("#input");
+  if (input.value == "") {
+    console.log("No input");
+    for (let i = 0; i < Drp.length; i++) {
+      Drp[i].style.display = "none";
+    }
+  }
+  else{
+    let result = input.value
+    
+    result = result.Capitalize();
+    // console.log(result);
+    
+    GetTracks(result);
+  }
+}
+
+
+function Dropdown(Data) {
+  Drp = document.getElementsByClassName("drp");
+  for (let i = 0; i < Drp.length; i++) {
+    Drp[i].style.display = "flex";
+  }
+  var dropdown;
+  for (let i = 0; i < 5; i++) {
+    console.log(Data.tracks.items[i].name);
+    dropdown = document.querySelector("#drp"+(i+1));
+    dropdown.innerHTML = Data.tracks.items[i].name+" - "+Data.tracks.items[i].artists[0].name 
+    dropdown.myParams = Data.tracks.items[i];
+    dropdown.addEventListener("click", Details);
+
+  }
+}
+var affiche = [];
+function Details(e) {
+  details = document.querySelector("#details");
+  let Data = e.target.myParams;
+  for (let i = 0; i < affiche.length; i++) {
+    if (affiche[i] == Data.name) {
+      // console.log(affiche[i]);
+      alert("Already here");
+      return;
+    }
+  }
+  details.innerHTML +=`<figure>
+    <figcaption>${Data.name}</figcaption>
+    <audio
+        controls
+        src="${Data.preview_url}">
+            <a href="${Data.preview_url}">
+                Download audio
+            </a>
+    </audio>
+</figure>`;
+affiche.push(Data.name);
+}
+
 SpotifyAuthentification();
